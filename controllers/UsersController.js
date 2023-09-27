a1e11e2df240e10fad09471c3c8d853d79806b24
Kauan Mello Cancelier <kauanmellocancelier2005@gmail.com>
2023-09-27 13:55:02 -0300

    Implementado validação inicial do metódo create de Usuarios.

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,10 +1,27 @@
+const BAD_REQUEST = 400;
+
 class UsersController {
 
     async index(request, response) {}
 
     async create(request, response) {
-        response.send("Pegando o corpo da request")
+
+        var {name, password, email} = request.body
+
+        if (email == undefined) {
+            return response.status(BAD_REQUEST).send("O email é obrigatório")
+        }
+
+        if (name == undefined || name.length < 3) {
+            return response.status(BAD_REQUEST).send("O nome é obrigatório e deve conter no ao menos 3 caracteres. ")
+        }
+
+        if (password == undefined || name.length < 3) {
+            return response.status(BAD_REQUEST).send("A senha é obrigatória e deve conter ao menos 3 caracteres.")
+        }
+
     }
+
 }
 
 module.exports = new UsersController()
